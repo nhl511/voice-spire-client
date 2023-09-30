ff901383e250c368596086c5fb1594145e130d5f
Tran Thanh Cong <tranthanhcong652001@gmail.com>
2023-09-30 23:33:29 +0700

    add component to path '/'

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/bank" element={<BankAccount />} />
         <Route path="/register" element={<RegisterSeller />} />
